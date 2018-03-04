c32144f17bf1067d1d2ed17f021a0e8ef285fe04
Lyndon Hebditch <lyndonhebditch@gmail.com>
2018-03-04 16:53:12 +0000

    Added pages and functionality
    -added additional pages(blank)
    -restructured js and css to be shared
    -trying some more design ideas

---
+++ main.js
@@ -0,0 +1,67 @@
+cfa = {
+    // functions for home page of cfa site
+    home: {
+        // functions for deciding which overlay to display
+        hexover1: function() { $('div.description.mid').css('opacity','1'); },
+        hexover2: function() { $('div.description.left').css('opacity','1'); },
+        hexover3: function() { $('div.description.right').css('opacity','1'); },
+        hexleave1: function() { $('div.description.mid').css('opacity','0'); },
+        hexleave2: function() { $('div.description.left').css('opacity','0'); },
+        hexleave3: function() { $('div.description.right').css('opacity','0'); },
+        
+        // init function 
+        init: function() {
+            // on mouse over of hexagon
+            $('img.hexagon').on('mouseover', function(e) {
+                var functions = [ cfa.home.hexover1, cfa.home.hexover2, cfa.home.hexover3 ];
+
+                var selhex = parseInt($(this).attr('data-id'));
+                functions[selhex - 1]();
+            });
+            // on mouse leave of hexagon
+            $('img.hexagon').on('mouseleave', function(e) {
+                var functions = [ cfa.home.hexleave1, cfa.home.hexleave2, cfa.home.hexleave3 ];
+
+                var selhex = parseInt($(this).attr('data-id'));
+                functions[selhex - 1]();
+            });
+            // on click of hexagon
+            $('article.landing-banner').on('click', function(e){
+                var page = $(e.currentTarget).attr('data-page');;
+
+                window.location.href = '/' + page + '.html';
+            });
+            
+            // center all components on page
+            var center = function() {
+                var container = $('div.container');
+                container.css('margin-top', ((window.innerHeight - container.height()) / 2) - 60);
+
+                var descriptionarea = $('div.description');
+                descriptionarea.css('width', (window.innerWidth - container.width()) / 2)
+            };
+            // center on resize
+            center();
+            window.onresize = center;
+        }
+    },
+    rig:{
+        init:function(){}
+    },
+    drum:{
+        init:function(){}
+    },
+    pump:{
+        init:function(){}
+    },
+    staff:{
+        init:function(){}
+    },
+    contact:{
+        init:function(){}
+    },
+    about:{
+        init:function(){}
+    }
+}
+
